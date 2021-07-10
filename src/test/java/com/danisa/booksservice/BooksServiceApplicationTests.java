4d38d700c309e22b5a61f31a352d16c395a88c09
dani <daninton92@gmail.com>
2021-07-10 08:37:08 +0200

    Initial commit

---
+++ src/test/java/com/danisa/booksservice/BooksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danisa.booksservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
